352f6dbb22df0dd79c0c21ba39b1cc13ea4fc2f3
amanda-santos <amandasf0200@gmail.com>
2021-04-28 20:35:07 -0300

    Add Homepage and Episode page

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -12,7 +12,33 @@ export function Player() {
         <strong>Selecione um podcast para ouvir</strong>
       </div>
 
-      <footer></footer>
+      <footer className={styles.empty}>
+        <div className={styles.progress}>
+          <span>00:00</span>
+          <div className={styles.slider}>
+            <div className={styles.emptySlider} />
+          </div>
+          <span>00:00</span>
+        </div>
+
+        <div className={styles.buttons}>
+          <button>
+            <img src="/shuffle.svg" alt="Embaralhar" />
+          </button>
+          <button>
+            <img src="/play-previous.svg" alt="Tocar anterior" />
+          </button>
+          <button type="button" className={styles.playButton}>
+            <img src="/play.svg" alt="Tocar" />
+          </button>
+          <button type="button">
+            <img src="/play-next.svg" alt="Tocar próxima" />
+          </button>
+          <button type="button">
+            <img src="/repeat.svg" alt="Repetir" />
+          </button>
+        </div>
+      </footer>
     </div>
   );
 }
